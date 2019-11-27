9e60212ad33a9ebd92ecab9a30c9a511ef16bbab
Jawad Rizvi <jawadrizvi40@gmail.com>
2019-11-26 17:32:13 -0700

    homepage path

---
+++ build/precache-manifest.1292105aa4d03be16d52bf7c8e53fee5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62a6399dbf4566d67a574ff75f2f0ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a3179c28fd444aa346a",
+    "url": "./static/css/main.8c16e140.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "./static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "5a3179c28fd444aa346a",
+    "url": "./static/js/main.2ac1dbdb.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "./static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "./static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
